867a32bd0adfffbc0871f317f8e008be551dbaa1
Saidatta <venkatamunnangi@gmail.com>
2012-02-24 13:36:21 -0800

    corrected the indentation of P030.java

--- P030.java
+++ P030.java
@@ -11,36 +11,35 @@
 	sys     0m0.015s
 */
 
-	public class P030
+public class P030
+{
+	public static void main(String [] args)
 	{
-		public static void main(String [] args)
-			{
-					long sum = 0;
-							for(int i = 2;i<531441;i++)
-										if(compute(i))
-														sum+=i;
+		long sum = 0;
 
-																/*
-																		The maximum value for one digit is 9^5 = 59049. We can find 			out the maximum possible sum for a given number of digits by 			multiplying 59049 with the number of digits.
-																				
-																						Let's say we're gonna check the number 123456789. That's 9 			digits, so the maximum sum would be 9*59049 = 531441
-																								*/
-																										System.out.println(sum);		
-																											}
-																												
-																													public static boolean compute(int x)
-																														{
-																																int y = x;
-																																		double s =0;
-																																				while(y>0)
-																																						{
-																																								 	s += Math.pow((y%10),5);
-																																												y /= 10;
-																																														}
+	for(int i = 2;i<531441;i++)								if(compute(i))
+			sum+=i;
 
-																																																if(x == s)
-																																																			return true;
-																																																					else 
-																																																								return false;
-																																																									}
-																																																									}
+										/*
+The maximum value for one digit is 9^5 = 59049. We can find 			out the maximum possible sum for a given number of digits by 			multiplying 59049 with the number of digits.
+																				Let's say we're gonna check the number 123456789. That's 9 			digits, so the maximum sum would be 9*59049 = 531441
+										*/
+	System.out.println(sum); // printing the sum.
+	
+}
+										public static boolean compute(int x)
+{
+											int y = x;
+	double s =0;
+	while(y>0)
+	{
+		s += Math.pow((y%10),5);
+												y /= 10;
+											}
+
+											if(x == s)
+		return true;
+	else 
+		return false;
+   }
+										}
